7b920a5f43d8cfccc4003b9d3d2bf1fedc9ef793
oscarprdev <vlcoscar8@gmail.com>
2025-04-06 10:51:42 +0200

    Add authentication middleware and dashboard page; implement route protection to ensure user is authenticated before accessing the dashboard.

---
+++ app/middleware/auth.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware(() => {
+  const session = useSupabaseSession();
+  if (!session.value) {
+    return navigateTo('/auth');
+  }
+});
---
+++ app/pages/dashboard.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ['auth'],
+});
+</script>
+
+<template>
+  <p>Dashboard</p>
+</template>
