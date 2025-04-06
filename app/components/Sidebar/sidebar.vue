6ce2aeba68fba1e2834545cbf3b81fc0f21b4413
oscarprdev <vlcoscar8@gmail.com>
2025-04-06 19:50:34 +0200

    feat: update Sidebar component to accept isOpen prop and integrate toggle functionality in dashboard layout

--- app/components/Sidebar/sidebar.vue
+++ app/components/Sidebar/sidebar.vue
@@ -1,30 +1,17 @@
 <script lang="ts" setup>
-import { PanelRightClose, PanelRightOpen } from 'lucide-vue-next';
 import { AnimatePresence, motion } from 'motion-v';
 
-const isSidebarOpen = ref(true);
-
-const handleToggleSidebarVisibility = () => {
-  isSidebarOpen.value = !isSidebarOpen.value;
-};
+defineProps<{ isOpen: boolean }>();
 </script>
 <template>
-  <div class="relative">
-    <button
-      class="absolute top-2 left-2 z-50"
-      @click="handleToggleSidebarVisibility">
-      <PanelRightOpen v-if="isSidebarOpen" />
-      <PanelRightClose v-else />
-    </button>
-
-    <AnimatePresence>
-      <motion.aside
-        :initial="{ x: 0 }"
-        :animate="{ x: isSidebarOpen ? 0 : -300 }"
-        :exit="{ x: -300 }"
-        :transition="{ duration: 0.3, ease: 'easeInOut' }"
-        class="h-full w-[300px] bg-red-500 z-0">
-      </motion.aside>
-    </AnimatePresence>
-  </div>
+  <AnimatePresence>
+    <motion.aside
+      :initial="{ x: 0 }"
+      :animate="{ x: isOpen ? 0 : -300 }"
+      :exit="{ x: -300 }"
+      :transition="{ duration: 0.3, ease: 'easeInOut' }"
+      class="relative h-full w-[300px] bg-red-500 z-0">
+      sidebar
+    </motion.aside>
+  </AnimatePresence>
 </template>
--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -1,15 +1,28 @@
 <script lang="ts" setup>
+import { PanelRightOpen, PanelRightClose } from 'lucide-vue-next';
 import Sidebar from '~/components/Sidebar/sidebar.vue';
 
 definePageMeta({
   middleware: ['auth'],
 });
+
+const isSidebarOpen = ref(false);
+
+const handleToggleSidebarVisibility = () => {
+  isSidebarOpen.value = !isSidebarOpen.value;
+};
 </script>
 
 <template>
   <NuxtLayout name="default">
-    <section class="h-screen w-screen flex">
-      <Sidebar />
+    <section class="relative h-screen w-screen flex bg-green-100">
+      <button
+        class="absolute top-4 left-4 z-50"
+        @click="handleToggleSidebarVisibility">
+        <PanelRightOpen v-if="isSidebarOpen" />
+        <PanelRightClose v-else />
+      </button>
+      <Sidebar :is-open="isSidebarOpen" />
       <slot />
     </section>
   </NuxtLayout>
