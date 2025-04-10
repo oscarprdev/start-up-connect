78a5991aa888b40e22226436f411e848ab9b942a
oscarprdev <vlcoscar8@gmail.com>
2025-04-10 08:07:17 +0200

    feat: refactor idea storage logic into a separate function for improved code organization and maintainability

--- server/api/ideas/index.post.ts
+++ server/api/ideas/index.post.ts
@@ -2,6 +2,12 @@ import { z } from 'zod';
 import { db } from '~~/server/db';
 import { ideasTable } from '~~/server/db/schemas';
 
+interface StoreIdeaParams {
+  title: string;
+  description: string;
+  userId: string;
+}
+
 const bodySchema = z.object({
   title: z.string(),
   description: z.string(),
@@ -10,16 +16,17 @@ const bodySchema = z.object({
 export default defineEventHandler(
   authMiddleware(async event => {
     const { title, description } = await readValidatedBody(event, bodySchema.parse);
-
-    const idea = await db
-      .insert(ideasTable)
-      .values({
-        title,
-        description,
-        userId: event.context.user.id,
-      })
-      .returning();
+    const idea = await storeIdea({ title, description, userId: event.context.user.id });
 
     return idea;
   })
 );
+
+const storeIdea = async ({ title, description, userId }: StoreIdeaParams) => {
+  const idea = await db.insert(ideasTable).values({
+    title,
+    description,
+    userId,
+  });
+  return idea;
+};
