ce55ef189f3a25a3501fa9a38c64341552316bed
oscarprdev <vlcoscar8@gmail.com>
2025-04-11 08:16:04 +0200

    refactor: restructure database configuration and introduce new schema and migration files for improved organization; add initial database tables for users, ideas, and related entities

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,8 @@ const connectionString = process.env.DATABASE_URL || '';
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './server/db/schemas.ts',
-  out: './server/db/migrations',
+  schema: './server/infra/db/schemas.ts',
+  out: './server/infra/db/migrations',
   dbCredentials: {
     url: connectionString,
   },
